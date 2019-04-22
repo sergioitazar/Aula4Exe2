2dfecfc1707edff503ea03eabb0e0a179091ddf5
sergioitazar <itazar@gmail.com>
2019-04-22 08:55:09 -0300

    Initial commit

---
+++ src/br/com/digitalhouse/PrincipalAula4Exec2.java
@@ -0,0 +1,15 @@
+package br.com.digitalhouse;
+
+public class PrincipalAula4Exec2 {
+
+    //exercicio 2
+
+    public static boolean verficamenor(int g, int h){
+        return g < h;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(verficamenor(20,10));
+    }
+
+}
